a8203a8f4393c4f2feb304e6fd6e89a87d53ec27
Ryan Dilley <ryandilley54@outlook.com>
2026-03-31 13:46:55 -0500

    made button print cats on click

--- button-console/script.js
+++ button-console/script.js
@@ -1,9 +1,3 @@
-
-var nameInput = document.querySelector("#name-input");
-var greetingOutput = document.querySelector("#greeting-output");
-
-function doTheThing() {
-  var name = nameInput.value;
-
-  greetingOutput.innerHTML = "Hello " + name + ", welcome home!";
+function handleClick() {
+  console.log("Cats");
 }
